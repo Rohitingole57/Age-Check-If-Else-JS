6f5b1d305441a56e775fb620e3248bbf974a77bf
Rohit Ingole <ingolerohit2017@gmail.com>
2023-02-14 11:41:35 +0530

    javascript age cheack if-else

---
+++ index.js
@@ -0,0 +1,21 @@
+var age = 17;
+var gender = " male";
+
+
+if (gender == "male") {
+    if (age > 21) {
+        console.log("can get married");
+    } else {
+        console.log("can not get married");
+
+    }
+} else{
+
+    if (age > 18) {
+        console.log("u are able to marry");
+
+    } else {
+        console.log("not able to marry");
+    }
+
+}
